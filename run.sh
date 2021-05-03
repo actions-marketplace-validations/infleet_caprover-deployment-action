7074d38794c4a8982fda3bb7fe3bdd371dc5460a
Rodolfo Silva <contato@rodolfosilva.com>
2021-05-03 10:57:09 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ -z ${INPUT_SERVER} ]] || [[ -z ${INPUT_PASSWORD} ]] || [[ -z ${INPUT_APPNAME} ]]; then
+if [[ -z ${INPUT_SERVER} ]] || [[ -z ${INPUT_PASSWORD} ]] || [[ -z ${INPUT_APP_NAME} ]]; then
   echo "Invalid input. Some required arguments missing."
   exit 1
 fi
 
 if [[ ! -z ${INPUT_TARBALL} ]]; then
-  caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APPNAME -t $INPUT_TARBALL
+  caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APP_NAME -t $INPUT_TARBALL
 elif [[ ! -z ${INPUT_BRANCH} ]]; then
-  caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APPNAME -b $INPUT_BRANCH
+  caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APP_NAME -b $INPUT_BRANCH
 else
   echo "Invalid input. Either a tarball or a branch argument is required."
   exit 1
